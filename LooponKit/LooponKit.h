458cc1aa3cef49f805f82326ac70db46337acf4c
Bruno Resende <bruno.resende@loopon.com>
2017-11-22 13:26:50 +0100

    Initial Commit

---
+++ LooponKit/LooponKit.h
@@ -0,0 +1,19 @@
+//
+//  LooponKit.h
+//  LooponKit
+//
+//  Created by Bruno Resende on 22/11/2017.
+//  Copyright © 2017 Loopon AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LooponKit.
+FOUNDATION_EXPORT double LooponKitVersionNumber;
+
+//! Project version string for LooponKit.
+FOUNDATION_EXPORT const unsigned char LooponKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LooponKit/PublicHeader.h>
+
+
